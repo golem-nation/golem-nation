30ea0219d8f6b31dba9eeb1a138c46f602c22ad8
Adam Oleszko <adamoleszko44@gmail.com>
2024-02-17 20:25:21 +0100

    Add icon

--- n8n/nodes/nodes/Golem/Golem.node.ts
+++ n8n/nodes/nodes/Golem/Golem.node.ts
@@ -5,13 +5,14 @@ import {
 	INodeTypeDescription,
 	NodeOperationError,
 } from 'n8n-workflow';
-import { TaskExecutor } from "@golem-sdk/golem-js";
-import * as fs from "fs";
+import { TaskExecutor } from '@golem-sdk/golem-js';
+import * as fs from 'fs';
 
 export class Golem implements INodeType {
 	description: INodeTypeDescription = {
 		displayName: 'Golem',
 		name: 'golem',
+		icon: 'file:golem.svg',
 		group: ['apps'],
 		version: 1,
 		description: 'Node to make some magic',
@@ -73,9 +74,9 @@ export class Golem implements INodeType {
 								description: 'Command to run',
 								displayOptions: {
 									show: {
-										command: ['run']
-									}
-								}
+										command: ['run'],
+									},
+								},
 							},
 							{
 								displayName: 'File Input Field',
@@ -85,9 +86,9 @@ export class Golem implements INodeType {
 								description: 'Field name of file in node input',
 								displayOptions: {
 									show: {
-										command: ['uploadFile']
-									}
-								}
+										command: ['uploadFile'],
+									},
+								},
 							},
 							{
 								displayName: 'Field Name in Output',
@@ -96,9 +97,9 @@ export class Golem implements INodeType {
 								default: '', // The initially selected option
 								displayOptions: {
 									show: {
-										command: ['run', 'downloadFile']
-									}
-								}
+										command: ['run', 'downloadFile'],
+									},
+								},
 							},
 							{
 								displayName: 'Path to File',
@@ -107,9 +108,9 @@ export class Golem implements INodeType {
 								default: '', // The initially selected option
 								displayOptions: {
 									show: {
-										command: ['downloadFile']
-									}
-								}
+										command: ['downloadFile'],
+									},
+								},
 							},
 							{
 								displayName: 'Mimetype of File',
@@ -118,9 +119,9 @@ export class Golem implements INodeType {
 								default: '', // The initially selected option
 								displayOptions: {
 									show: {
-										command: ['downloadFile']
-									}
-								}
+										command: ['downloadFile'],
+									},
+								},
 							},
 							{
 								displayName: 'Filetype of File',
@@ -129,13 +130,12 @@ export class Golem implements INodeType {
 								default: '', // The initially selected option
 								displayOptions: {
 									show: {
-										command: ['downloadFile']
-									}
-								}
-							}
+										command: ['downloadFile'],
+									},
+								},
+							},
 						],
 					},
-
 				],
 			},
 		],
@@ -156,76 +156,85 @@ export class Golem implements INodeType {
 		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
 			try {
 				item = items[itemIndex];
-				const commands = this.getNodeParameter('golemCommands', itemIndex, '') as {commandsValues: {command: string; commandText: string}[]}
-				const container = this.getNodeParameter('container', itemIndex, '') as string
+				const commands = this.getNodeParameter('golemCommands', itemIndex, '') as {
+					commandsValues: { command: string; commandText: string }[];
+				};
+				const container = this.getNodeParameter('container', itemIndex, '') as string;
 
-				console.log(commands.commandsValues)
+				console.log(commands.commandsValues);
 				// myString = this.getNodeParameter('myString', itemIndex, '') as string;
 
 				// item.json['myString'] = myString;
 				item.json['command'] = commands.commandsValues;
 
-
 				// GOLEM START
 
 				// const executor = await TaskExecutor.create("golem/alpine:latest");
-				  const executor = await TaskExecutor.create(container);
-  			try {
-						await executor.run(async (ctx) => {
-							for (const commandObj of commands.commandsValues) {
-								if (commandObj.command === 'run') {
-									const result = (await ctx.run(commandObj.commandText)).stdout
-									// @ts-ignore
-									item.json[commandObj.outputFieldName] = result
-									//
-									// if(item.json['commandOutputs']) {
-									// 	item.json['commandOutputs'].push(result)
-									// }  else {
-									// 	item.json['commandOutputs'] = [result]
-									// }
-								}
-
-								if (commandObj.command === 'uploadFile') {
-									//@ts-ignore
-									const inputFileField = commandObj.fileInputField
-									const file = item?.binary?.[inputFileField]
-									if (!file) continue;
-									const currentDirectory = process.cwd()
-									//@ts-ignore
-									fs.writeFileSync(file.fileName, file.data, {encoding: "base64"})
-									await ctx.uploadFile(`${currentDirectory}/${file.fileName}`, `/golem/work/${file.fileName}`)
-
-									console.log('UPLOADED')
-								}
-
-								if (commandObj.command === 'downloadFile') {
-									const currentDirectory = process.cwd()
-									//@ts-ignore
-									const filePath = commandObj.filePath
-									const splitFilePath =filePath.split('/')
-									const fileName = splitFilePath[splitFilePath.length - 1]
-      						await ctx.run("cp /golem/work/File.csv /golem/output/File.csv")
-									await ctx.downloadFile(filePath, fileName)
-									const file = fs.readFileSync(`${currentDirectory}/${fileName}`)
-									//@ts-ignore
-									const outputFieldName = commandObj.outputFieldName
-
-									item.binary = {
-										...(item.binary || {}),
-										//@ts-ignore
-										[outputFieldName]: {mimeType: commandObj.mimeType, data: file.toString('base64'), fileType: commandObj.fileType, fileName: 'superFile.csv'}
-									}
-								}
+				const executor = await TaskExecutor.create(container);
+				try {
+					await executor.run(async (ctx) => {
+						for (const commandObj of commands.commandsValues) {
+							if (commandObj.command === 'run') {
+								const result = (await ctx.run(commandObj.commandText)).stdout;
+								// @ts-ignore
+								item.json[commandObj.outputFieldName] = result;
+								//
+								// if(item.json['commandOutputs']) {
+								// 	item.json['commandOutputs'].push(result)
+								// }  else {
+								// 	item.json['commandOutputs'] = [result]
+								// }
 							}
-							console.log('DONE')
-					})
+
+							if (commandObj.command === 'uploadFile') {
+								//@ts-ignore
+								const inputFileField = commandObj.fileInputField;
+								const file = item?.binary?.[inputFileField];
+								if (!file) continue;
+								const currentDirectory = process.cwd();
+								//@ts-ignore
+								fs.writeFileSync(file.fileName, file.data, { encoding: 'base64' });
+								await ctx.uploadFile(
+									`${currentDirectory}/${file.fileName}`,
+									`/golem/work/${file.fileName}`,
+								);
+
+								console.log('UPLOADED');
+							}
+
+							if (commandObj.command === 'downloadFile') {
+								const currentDirectory = process.cwd();
+								//@ts-ignore
+								const filePath = commandObj.filePath;
+								const splitFilePath = filePath.split('/');
+								const fileName = splitFilePath[splitFilePath.length - 1];
+								await ctx.run('cp /golem/work/File.csv /golem/output/File.csv');
+								await ctx.downloadFile(filePath, fileName);
+								const file = fs.readFileSync(`${currentDirectory}/${fileName}`);
+								//@ts-ignore
+								const outputFieldName = commandObj.outputFieldName;
+
+								item.binary = {
+									...(item.binary || {}),
+									[outputFieldName]: {
+										//@ts-ignore
+										mimeType: commandObj.mimeType,
+										data: file.toString('base64'),
+										//@ts-ignore
+										fileType: commandObj.fileType,
+										fileName: 'superFile.csv',
+									},
+								};
+							}
+						}
+						console.log('DONE');
+					});
 				} catch (error) {
-					console.error("Computation failed:", error);
+					console.error('Computation failed:', error);
 				} finally {
 					await executor.shutdown();
-					console.log('SHUTDOWN')
+					console.log('SHUTDOWN');
 				}
-
 
 				// GOLEM END
 			} catch (error) {

8d1035194a22ad326890ffbac45135cfb0560980
ambrozini <ambro.szymon@gmail.com>
2024-02-17 19:27:29 +0100

    Use proper api key env variable

--- n8n/entrypoint.sh
+++ n8n/entrypoint.sh
@@ -5,7 +5,7 @@ export NODE_ID=`yagna id create --from-keystore ./key.json --json | jq ".Ok.node
 #export NODE_ID=`yagna id show --json | jq ".Ok.nodeId"`
 yagna id update --set-default $NODE_ID
 echo "asd" | yagna id unlock
-export APP_KEY=`yagna app-key create new-requestor`
+export YAGNA_APPKEY=`yagna app-key create new-requestor`
 
 n8n
 
